5fcf59c234969f7a9d321df26f0fa7afe399a7ca
Snokke <snakemgl.ukraine@gmail.com>
2023-10-11 23:45:50 +0300

    Change color

--- src/scene/game-scene/game-scene.js
+++ src/scene/game-scene/game-scene.js
@@ -53,7 +53,7 @@ export default class GameScene extends THREE.Group {
     this._initSignals();
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
     const cube = new THREE.Mesh(geometry, material);
     this.add(cube);
   }
